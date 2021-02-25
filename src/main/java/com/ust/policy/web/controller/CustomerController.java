5970f741cc308476bd8bd275988a15cf01906bb3
Cristhian Castaneda <ccastanedarivera@gmail.com>
2021-02-25 06:09:02 -0600

    add create service

--- src/main/java/com/ust/policy/web/controller/CustomerController.java
+++ src/main/java/com/ust/policy/web/controller/CustomerController.java
@@ -1,6 +1,7 @@
 package com.ust.policy.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,10 @@ public class CustomerController {
     @GetMapping
     public Map getAll() {
         return new HashMap();
+    }
+
+    @PostMapping
+    public void save() {
     }
 
 }

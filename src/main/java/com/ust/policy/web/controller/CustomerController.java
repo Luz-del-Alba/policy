c77ab0d257eb545494d711914526ac7894deef41
Cristhian Castaneda <ccastanedarivera@gmail.com>
2021-02-25 04:07:30 -0600

    refactor name-module

--- src/main/java/com/ust/policy/web/controller/CustomerController.java
+++ src/main/java/com/ust/policy/web/controller/CustomerController.java
@@ -1,4 +1,4 @@
-package com.ust.customer.web.controller;
+package com.ust.policy.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/test/java/com/ust/policy/PolicyApplicationTests.java
+++ src/test/java/com/ust/policy/PolicyApplicationTests.java
@@ -1,10 +1,10 @@
-package com.ust.customer;
+package com.ust.policy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CustomerApplicationTests {
+class PolicyApplicationTests {
 
 	@Test
 	void contextLoads() {
